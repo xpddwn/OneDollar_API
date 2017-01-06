54a883e6c14faa03f2f160eae2566a165d760b1e
isiki <isiki@buaa.edu.cn>
2017-01-06 12:10:11 +0800

    购物记录中增加余额不足不能付款的逻辑

--- sku/views.py
+++ sku/views.py
@@ -6,7 +6,7 @@ from sku.filters import GoodsFilter, JsonOrderingFilter, JsonDjangoFilterBackend
     ShoppingRecordFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-
+from rest_framework import status
 
 class SKUViewSet(ModelViewSet):
     queryset = SKU.objects.all()
@@ -51,3 +51,20 @@ class ShoppingRecordViewSet(ModelViewSet):
                        JsonDjangoFilterBackend,
                        filters.SearchFilter)
     filter_class = ShoppingRecordFilter
+
+    def create(self, request, *args, **kwargs):
+        user = User.objects.get(id=request.data['user'])
+        sku = SKU.objects.get(id=request.data['sku'])
+        if user.balance - sku.goods.price < 0:
+            return Response({"error": "insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
+        data = dict()
+        data['user'] = user.id
+        data['sku'] = sku.id
+        data['number'] = sku.number
+        data['payment'] = sku.goods.price
+        serializer = ShoppingRecordSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user.balance -= sku.goods.price
+        user.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
