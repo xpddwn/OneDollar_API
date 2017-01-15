5343485ca768dd23de34d6d473967c9e418b0e2e
isiki <isiki@buaa.edu.cn>
2017-01-15 16:57:09 +0800

    用户登录权限部分调整

--- sku/views.py
+++ sku/views.py
@@ -13,6 +13,12 @@ from rest_framework import status, viewsets, mixins
 from django.contrib.auth.hashers import make_password
 
 
+def check_login(request, user):
+    if request.session.get('user') == user:
+        return True
+    return False
+
+
 class SKUViewSet(ModelViewSet):
     queryset = SKU.objects.all()
     serializer_class = SKUSerializer
@@ -122,13 +128,14 @@ class ShoppingRecordViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         user = User.objects.get(id=request.data['user'])
+        if not check_login(request, user.id):
+            return Response({"error": "not logged in"}, status=status.HTTP_400_BAD_REQUEST)
         sku = SKU.objects.get(id=request.data['sku'])
         amount = request.data.get('amount')
         sta = request.data.get('status')
         if amount is None or amount == 0:
             return Response({"error": "invalid amount"}, status=status.HTTP_400_BAD_REQUEST)
-        record = ShoppingRecord(user=user, sku=sku, number=self._set_number(),
-                                amount=amount, status=sta)
+        record = ShoppingRecord(user=user, sku=sku, amount=amount, status=sta)
         if sta == 1:
             success = self._pay(user, record)
             if success == 1:
@@ -136,6 +143,7 @@ class ShoppingRecordViewSet(ModelViewSet):
                                 status=status.HTTP_400_BAD_REQUEST)
             if success == 2:
                 return Response({"error": "sold out"}, status=status.HTTP_400_BAD_REQUEST)
+            record.number = self._set_number()
         record.save()
         return Response(ShoppingRecordSerializer(record).data,
                         status=status.HTTP_201_CREATED)
@@ -143,7 +151,9 @@ class ShoppingRecordViewSet(ModelViewSet):
     @list_route(methods=['post'], url_path='pay')
     def pay(self, request):
         user = User.objects.get(id=request.data.get('user'))
-        record = ShoppingRecord.objects.get(id=request.data.get('record'))
+        if not check_login(request, user.id):
+            return Response({"error": "not logged in"}, status=status.HTTP_400_BAD_REQUEST)
+        record = ShoppingRecord.objects.get(id=request.data.get('record'), user_id=user.id)
         success = self._pay(user, record)
         if success == 1:
             return Response({"error": "insufficient funds"},
@@ -151,8 +161,18 @@ class ShoppingRecordViewSet(ModelViewSet):
         if success == 2:
             return Response({"error": "sold out"}, status=status.HTTP_400_BAD_REQUEST)
         record.status = 1
+        record.number = self._set_number()
         record.save()
         return Response({"error": 0, "data": "succeed"}, status=status.HTTP_200_OK)
+
+    @list_route(methods=['get'], url_path='number')
+    def get_number(self, request):
+        sku_id = request.query_params.get('sku')
+        user_id = request.session.get('user')
+        if not ShoppingRecord.objects.filter(sku_id=sku_id, user_id=user_id, status__gte=1).exists():
+            return Response({"error": "no permission"}, status=status.HTTP_400_BAD_REQUEST)
+        number_list = ShoppingRecord.objects.filter(sku_id=sku_id).values_list("number", flat=True)
+        return Response({"data": number_list}, status=status.HTTP_200_OK)
 
 
 class RechargeInfoViewSet(ModelViewSet):
